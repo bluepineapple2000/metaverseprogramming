c5f5307b76b0fb24c066134de4b4b3bfa15b24f6
Natalie Boehm <natalieboehm@MacBook-Pro-4.local>
2023-03-24 15:21:21 +0900

    add Get Set Value files

---
+++ Assets/Scenes/weel03_GetSetValues/AnotherScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherScript : MonoBehaviour
+{
+    public string myVar = "this is my var";
+
+   
+    public void MyFunction()
+    {
+
+    }
+}
---
+++ Assets/Scenes/weel03_GetSetValues/week03_DriveMyCube.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class week03_DriveMyCube : MonoBehaviour
+{
+    float speed = 0.5f;
+    int dir = 1;
+    int score = 0;
+    public GameObject Score; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //string s = Score.GetComponent<TMP_Text>().text;
+        //print(s); 
+        Score.GetComponent<TMP_Text>().text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseH = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseH, 0); 
+        transform.Translate(0, 0, speed * dir * 0.01f);
+    }
+
+    // move forward
+    public void MoveForward()
+    {
+        //transform.Translate(0, 0, 1);
+        dir = 1;
+    }
+    // move backward
+    public void MoveBackward()
+    {
+        //transform.Translate(0, 0, -1);
+        dir = - 1;
+    }
+
+
+    public void Stop()
+    {
+        dir = 0; 
+    }
+    // turn right 
+    public void TurnRight()
+    {
+        transform.Rotate(0, 10, 0);
+    }
+
+    // turn left
+    public void TurnLeft()
+    {
+        transform.Rotate(0, -10, 0);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.tag);
+        if(collision.gameObject.tag == "MySphere")
+        {
+            //print("you get +1");
+                score++;
+            print(score);
+            Score.GetComponent<TMP_Text>().text = score.ToString();
+        }
+        if (collision.gameObject.tag == "MyCapsule")
+        {
+            //print("you get -1");
+            score--;
+            print(score);
+            if(score < 0)
+            {
+                score = 0;
+                SceneManager.LoadScene("week03_DriveMyCube");
+            }
+            Score.GetComponent<TMP_Text>().text = score.ToString();
+        }
+    }
+}
